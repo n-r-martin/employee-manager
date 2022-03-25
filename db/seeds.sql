3cbe85d363225b3e1e886d808fa8fa16d497f597
Nick Martin <martin.nicholas.r@gmail.com>
2022-03-24 19:30:18 -0600

    adding base inquirer script - tested and working - building out remaining prompts

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,20 @@
 INSERT INTO department (name)
-VALUES ("Reearch and Development"),
+VALUES ("Research and Development"),
        ("Engineering"),
-       ("Biological"),
+       ("Sciences"),
        ("Human Resources");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Analyst", 90000, 1),
        ("Engineer", 120000, 2),
-       ("Biologist", 80000, 3),
+       ("Ecologist", 80000, 3),
        ("Safety Supervisor", 100000, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Leto", "Atreides", 2, NULL),
+       ("Paul", "Atreides", 2, 1),
+       ("Duncan","Idaho", 4, 1),
+       ("Thufir", "Hawat", 1, 1),
+       ("Gurney", "Halleck", 4, 2),
+       ("Liet", "Kynes", 3, NULL);
        
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 // Import and require mysql2
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+// Import and require inquirer
+const inquirer = require("inquirer");
 // Import and require console.table
-require('console.table');
+require("console.table");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,13 +16,22 @@ app.use(express.json());
 // Connect to database
 const db = mysql.createConnection(
   {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'rootROOT88{}',
-    database: 'employees_db'
+    host: "127.0.0.1",
+    user: "root",
+    password: "rootROOT88{}",
+    database: "employees_db",
   },
   console.log(`Connected to the employees_db database.`)
 );
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // // Hardcoded query: DELETE FROM course_names WHERE id = 3;
 
@@ -36,24 +47,106 @@ const db = mysql.createConnection(
 //   console.table(results);
 // });
 
-db.promise().query('SELECT * FROM department').then((results) => {
-  console.table(results[0]);
-});
+// db.promise()
+//   .query("SELECT * FROM department")
+//   .then((results) => {
+//     console.table(results[0]);
+//   });
 
-db.promise().query('SELECT * FROM role').then((results) => {
-  console.table(results[0]);
-});         
+// db.promise()
+//   .query("SELECT * FROM role")
+//   .then((results) => {
+//     console.table(results[0]);
+//   });
+
+// db.promise()
+//   .query("SELECT * FROM employee")
+//   .then((results) => {
+//     console.table(results[0]);
+//   });
+
+// db.promise()
+//   .query(
+//     'SELECT d.name as "department name", r.title as "role title" FROM department d JOIN role r ON d.id=r.department_id WHERE d.id = 1'
+//   )
+//   .then((results) => {
+//     console.table(results[0]);
+//   });
+
+const basePrompt = () => {
+  return inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "baseSelection",
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit application'
+      ],
+    },
+  ]);
+};
+
+const questionsStart = () => {
+  basePrompt()
+  .then((response) => {
+
+    switch(response.baseSelection) {
+      case 'View all departments':
+        db.promise()
+          .query("SELECT * FROM department")
+          .then((results) => {
+            console.table(results[0]);
+            questionsStart();
+          });
+      break;
+      case 'View all roles':
+        db.promise()
+          .query("SELECT * FROM role")
+          .then((results) => {
+            console.table(results[0]);
+            questionsStart();
+          });
+      break;
+      case 'View all employees':
+        db.promise()
+          .query("SELECT * FROM employee")
+          .then((results) => {
+            console.table(results[0]);
+            questionsStart();
+          });
+      break;
+      default:
+        console.log(
+          " ---------------------------", '\n', 
+          "Goodbye!", '\n', 
+          "---------------------------"
+          )
+       process.exit();
+    }
+  })
+}
 
 
-db.promise().query('SELECT * FROM employee').then((results) => {
-  console.table(results[0]);
-});
+const init = () => {
+  console.log(
+    " -------------------------------------------", '\n', 
+    "(0_0) Welcome to the Employee Manager (0_0)", '\n', 
+    "-------------------------------------------"
+    )
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
+  questionsStart();
+}
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+///// APPLICATION GO BRRRRRRR >>>>>>
+init();
+
+
+
+ 
